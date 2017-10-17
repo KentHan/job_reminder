eed9f0b98c6e896a42ad03491ed14310181cc149
KentHan <kenthan.tw@gmail.com>
2017-10-18 00:40:07 +0800

    Add query_job_by_title() and reformat

--- dao.py
+++ dao.py
@@ -13,32 +13,39 @@ mongodb_pw = os.getenv("MONGODB_PW")
 mongodb_uri = os.getenv("MONGODB_URI")
 
 class JobDAO():
-	def __init__(self, client=None):
-		if client is None:
-			self.client = MongoClient(mongodb_uri)
-			self.client.admin.authenticate(mongodb_id, mongodb_pw, mechanism='SCRAM-SHA-1')
-		else:
-			self.client = client
-		self.db = self.client.user_data
+    def __init__(self, client=None):
+    	if client is None:
+    		self.client = MongoClient(mongodb_uri)
+    		self.client.admin.authenticate(mongodb_id, mongodb_pw, mechanism='SCRAM-SHA-1')
+    	else:
+    		self.client = client
+    	self.db = self.client.user_data
 
-	def query_all_jobs(self):
-		cursor = self.db.job.find()
-		return list(cursor)
+    def query_all_jobs(self):
+    	cursor = self.db.job.find()
+    	return list(cursor)
 
-	def add_job(self, job):
-		result = self.db.job.insert_one(
+    def query_job_by_title(self, job_title):
+        cursor = self.db.job.find(
+            {
+                "job_title": job_title,
+            })
+        return list(cursor)[0]        
+
+    def add_job(self, job):
+    	result = self.db.job.insert_one(
             {
                 "job_title": job['job_title'],
                 "company_name": job['company_name'],
                 "job_link": job['job_link']
             }
         )
-		return result.acknowledged
+    	return result.acknowledged
 
-	def delete_job(self, job):
-		result = self.db.job.delete_one(
+    def delete_job(self, job):
+    	result = self.db.job.delete_one(
             {
                 "job_title": job['job_title']
             }
         )
-		return result.acknowledged
+    	return result.acknowledged
